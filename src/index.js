9524b38db80882bd5dd4bf73178228748554b8ef
Frederico A Lacs <fredlacs@gmail.com>
2018-09-30 10:27:37 +0100

    added web3 object

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 
 import indexRoutes from "routes/index.jsx";
 
+import Web3 from 'web3';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
@@ -21,3 +23,6 @@ ReactDOM.render(
   </HashRouter>,
   document.getElementById("root")
 );
+
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+web3.eth.getAccounts().then(console.log);
