0010d4794eaedb0049423a539757694f0cf10307
Frederico A Lacs <fredlacs@gmail.com>
2018-10-01 11:48:57 +0100

    added necessary changes for truffle deployment, TODO: transfer ownership

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,27 @@
+var Escrow = artifacts.require("./Escrow");
+var KOLogic = artifacts.require("./KOLogic");
+var KolStorageV1 = artifacts.require("./KolStorageV1");
+var OwnableLogic = artifacts.require("./Ownable");
+var OwnableStorage = artifacts.require("./Ownable");
+var PullPayment = artifacts.require("./PullPayment");
+var SafeMath = artifacts.require("./SafeMath");
+
+module.exports = function(deployer) {
+
+  deployer.deploy([
+    Escrow,
+    OwnableLogic,
+    OwnableStorage,
+    PullPayment,
+    SafeMath
+  ]);
+  
+  deployer.link(OwnableStorage, KolStorageV1);
+  deployer.link(OwnableLogic, KOLogic);
+  deployer.link(PullPayment, KOLogic);
+
+  deployer.deploy(KolStorageV1).then(function() {
+    return deployer.deploy(KOLogic, 1, KolStorageV1.address)    
+  });
+  
+};
