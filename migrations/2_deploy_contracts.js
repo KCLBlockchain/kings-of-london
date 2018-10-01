a38931566d57c49442166b87db6d2d6efb260839
Frederico A Lacs <fredlacs@gmail.com>
2018-10-01 12:42:27 +0100

    small bug fixes, need to test if libs are correctly linked

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 var Escrow = artifacts.require("./Escrow");
 var KOLogic = artifacts.require("./KOLogic");
 var KolStorageV1 = artifacts.require("./KolStorageV1");
-var OwnableLogic = artifacts.require("./OwnableLogic");
-var OwnableStorage = artifacts.require("./OwnableStorage");
+var OwnableLogic = artifacts.require("./Ownable");
+var OwnableStorage = artifacts.require("./Ownable");
 var PullPayment = artifacts.require("./PullPayment");
 var SafeMath = artifacts.require("./SafeMath");
 
@@ -18,8 +18,7 @@ module.exports = function(deployer) {
   
   deployer.link(OwnableStorage, KolStorageV1);
   deployer.link(OwnableLogic, KOLogic);
-  deployer.link(PullPayment, KOLogic);
-
+  
   deployer.deploy(KolStorageV1).then(function() {
     return deployer.deploy(KOLogic, 1, KolStorageV1.address)    
   });
